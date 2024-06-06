93653b29ab29fe32c666165ca132d6673f68d1f8
Thiago Souza <thiagorasouza@gmail.com>
2024-06-05 23:09:46 -0500

    feat: implement left side bar

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Manrope } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const manrope = Manrope({ subsets: ["latin"] });
