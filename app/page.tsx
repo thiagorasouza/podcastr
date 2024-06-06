f422b3530d0265c8391b1f74362bead257088c17
Thiago Souza <thiagorasouza@gmail.com>
2024-06-06 18:57:04 -0500

    feat: implement trending podcasts section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React from "react";
 import { manrope } from "./fonts";
 import { cn } from "@/lib/utils";
-import { sidebarLinks } from "@/constants";
+import { podcastData, sidebarLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -12,13 +12,11 @@ const Home = () => {
   const pathname = usePathname();
 
   return (
-    <div className="flex w-full">
-      <aside className="left_sidebar">
-        <header className="flex gap-2 items-center mb-12">
+    <div className={cn("flex w-full", manrope.className)}>
+      <aside className="left_sidebar min-h-screen justify-start">
+        <header className="flex gap-2 items-center mb-14">
           <Image src="/icons/logo.svg" alt="logo" width={28} height={28} />
-          <h1 className={cn(manrope.className, "text-24 font-extrabold")}>
-            Podcastr
-          </h1>
+          <h1 className="text-24 font-extrabold">Podcastr</h1>
         </header>
         <nav>
           <ul className="flex flex-col gap-4 text-white-2 text-16">
@@ -46,7 +44,26 @@ const Home = () => {
           </ul>
         </nav>
       </aside>
-      <main className="flex-1">Main</main>
+      <main className="flex-1 py-10 px-8">
+        <section>
+          <h2 className="text-20 font-bold mb-5">Trending Podcasts</h2>
+          <div className="podcast_grid">
+            {podcastData.map(({ id, title, description, imgURL }) => (
+              <article key={id} className="flex flex-col">
+                <Image
+                  src={imgURL}
+                  alt="podcast thumbnail"
+                  width={174}
+                  height={174}
+                  className="mb-2 w-full aspect-square object-cover rounded-md"
+                />
+                <h3 className="text-16 font-bold">{title}</h3>
+                <p className="text-12 capitalize">{description}</p>
+              </article>
+            ))}
+          </div>
+        </section>
+      </main>
       <aside className="right_sidebar">Right</aside>
     </div>
   );
